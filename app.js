3387c860646b9cec5b435564b56cf5fa85f9732d
444Jocke <joakim.karlsson.ken@gmail.com>
2022-03-15 10:59:08 +0100

    gjorde om navbar

--- app.js
+++ app.js
@@ -14,22 +14,3 @@ window.onscroll = function () {
     }
   }
 
-  let counterDisplayElem = document.querySelector('.counter-display');
-  let counterMinusElem = document.querySelector('.counter-minus');
-  let counterPlusElem = document.querySelector('.counter-plus');
-  
-  let count = 0;
-  
-  updateDisplay();
-  
-  let count(){
-    // do whatever you like here
-
-    setTimeout(yourFunction, 5000);
-}
-
-yourFunction();
-  
-  function updateDisplay(){
-      counterDisplayElem.innerHTML = count;
-  };
